46a9fba15affaac62dd8f471701868c7aba08335
Roberto Capancioni <roberto.capancioni@gmail.com>
2024-03-19 10:25:54 +0100

    Update d07_6_documento_tvw.sql

--- D07/d07_6_documento_tvw.sql
+++ D07/d07_6_documento_tvw.sql
@@ -13,12 +13,12 @@ instead of insert on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.insert_row (
-            p_id                          => null,
-            p_tipo_documento_id           => null,
-            p_codice                      => null,
-            p_data_documento              => null,
-            p_firma_yn                    => null,
-            p_categoria                   => null
+            p_id                          => id                     ,
+            p_tipo_documento_id           => tipo_documento_id      ,
+            p_codice                      => codice                 ,
+            p_data_documento              => data_documento         ,
+            p_firma_yn                    => firma_yn               ,
+            p_categoria                   => categoria              
             );
 end; 
 /
@@ -28,12 +28,12 @@ instead of update on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.update_row (
-            p_id                          => null,
-            p_tipo_documento_id           => null,
-            p_codice                      => null,
-            p_data_documento              => null,
-            p_firma_yn                    => null,
-            p_categoria                   => null
+            p_id                          => id                     ,
+            p_tipo_documento_id           => tipo_documento_id      ,
+            p_codice                      => codice                 ,
+            p_data_documento              => data_documento         ,
+            p_firma_yn                    => firma_yn               ,
+            p_categoria                   => categoria              
             );
 end; 
 /
@@ -43,7 +43,7 @@ instead of delete on d07_documento_tvw
 for each row 
 begin 
         d07_documento_api.delete_row (
-        p_id                           => null
+        p_id                           => id
     );
 end; 
 /
